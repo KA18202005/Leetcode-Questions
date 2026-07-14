dafe93f1bc8b17bc508f0d5644d31a64010718d0
Kavya Agarwal <kavyaag1617@gmail.com>
2026-07-14 12:45:20 +0530

    Time: 0 ms (100%), Space: 65 MB (95.68%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> vis(n + 1, 0);
+        int m = trust.size();
+        for(int i = 0; i < m; i++) {
+            vis[trust[i][0]]--;
+            vis[trust[i][1]]++;
+        }
+        for(int i = 1; i <= n; i++) {
+            if(vis[i] == n - 1) return i;
+        }
+        return -1;
+    }
+};
