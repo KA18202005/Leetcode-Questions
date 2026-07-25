a398185d5224400ac7ef6eaa09fca5f9bfd999be
Kavya Agarwal <kavyaag1617@gmail.com>
2026-07-25 07:45:37 +0530

    Time: 1558 ms (26.61%), Space: 49.3 MB (98.26%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<int> nums;
+public:
+    Solution(vector<int>& nums) {
+        this->nums.swap(nums);
+    }
+    
+    int pick(int target) {
+        int n = nums.size();
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                count++;
+                if (rand() % count == 0) idx = i;
+            }
+        }
+        return idx;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
