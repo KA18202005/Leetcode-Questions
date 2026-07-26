24b703f67cd0e0bb1e74cb8957719508a4d3fae9
Kavya Agarwal <kavyaag1617@gmail.com>
2026-07-26 15:36:03 +0530

    Time: 7 ms (41.22%), Space: 11.9 MB (80.49%) - LeetHub

---
+++ 1117-building-h2o/1117-building-h2o.cpp
@@ -0,0 +1,31 @@
+class H2O {
+private:
+    mutex m;
+    condition_variable cv;
+    int turn;
+
+public:
+    H2O() {
+        turn = 0;
+    }
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        unique_lock<mutex> lock(m);
+        while (turn == 2) {
+            cv.wait(lock);
+        }
+        releaseHydrogen();
+        ++turn;
+        cv.notify_all();
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        unique_lock<mutex> lock(m);
+        while (turn < 2) {
+            cv.wait(lock);
+        }
+        releaseOxygen();
+        turn = 0;
+        cv.notify_all();
+    }
+};
