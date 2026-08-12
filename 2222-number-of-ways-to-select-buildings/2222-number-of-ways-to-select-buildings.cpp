6206d250337da1086d11e2af67dbc021e81e8e6f
Kavya Agarwal <kavyaag1617@gmail.com>
2026-08-12 09:29:05 +0530

    Time: 8 ms (97.14%), Space: 27.8 MB (55.99%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     long long numberOfWays(string s) {
-        long long dp[4][2] = {};
+        long long cnt0 = 0;
+        long long cnt1 = 0;
+        long long cnt01 = 0;
+        long long cnt10 = 0;
+        long long cnt010 = 0;
+        long long cnt101 = 0;
         for (char c : s) {
-            int x = c - '0';
-            for (int j = 3; j >= 1; j--) {
-                dp[j][x] += dp[j - 1][1 - x];
-                if (j == 1) dp[j][x]++;
+            if (c == '0') {
+                cnt010 += cnt01;
+                cnt10 += cnt1;
+                cnt0++;
+            }
+            else {
+                cnt101 += cnt10;
+                cnt01 += cnt0;
+                cnt1++;
             }
         }
-        return dp[3][0] + dp[3][1];
+        return cnt010 + cnt101;
     }
 };
