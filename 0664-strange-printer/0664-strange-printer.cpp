38491b29bb9f4d8e89a3dee695b0e162eed47087
Kavya Agarwal <kavyaag1617@gmail.com>
2026-08-13 11:04:43 +0530

    Time: 13 ms (80.34%), Space: 12.2 MB (57.03%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = i + 1; j < n; j++) {
+                dp[i][j] = 1 + dp[i + 1][j];
+                for(int k = i + 1; k <= j; k++) {
+                    if(s[i] == s[k]) {
+                        int middle = 0;
+                        if(k > i + 1) middle = dp[i + 1][k - 1];
+                        dp[i][j] = min(dp[i][j], middle + dp[k][j]);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
