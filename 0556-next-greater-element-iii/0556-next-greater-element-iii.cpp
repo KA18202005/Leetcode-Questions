af798ce20df269d0fed29abbad541ca98c8f8858
Kavya Agarwal <kavyaag1617@gmail.com>
2026-08-25 06:51:56 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.99%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -2,7 +2,18 @@ class Solution {
 public:
     int nextGreaterElement(int n) {
         string s = to_string(n);
-        if(!next_permutation(s.begin(), s.end())) return -1;
+        int len = s.size();
+        int i = len - 2;
+        while(i >= 0 && s[i] >= s[i + 1]) {
+            i--;
+        }
+        if(i < 0) return -1;
+        int j = len - 1;
+        while(s[j] <= s[i]) {
+            j--;
+        }
+        swap(s[i], s[j]);
+        reverse(s.begin() + i + 1, s.end());
         long long ans = stoll(s);
         if(ans > INT_MAX) return -1;
         return (int)ans;
