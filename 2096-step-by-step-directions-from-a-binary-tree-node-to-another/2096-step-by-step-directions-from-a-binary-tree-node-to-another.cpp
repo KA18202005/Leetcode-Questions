ff89d509e6530d95d135224c768afbb25f592799
Kavya Agarwal <kavyaag1617@gmail.com>
2026-08-30 12:24:36 +0530

    Time: 12 ms (76.51%), Space: 211.7 MB (44.3%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findPath(TreeNode* root, int value, string& path) {
+        if(root == NULL) return false;
+        if(root->val == value) return true;
+        path.push_back('L');
+        if(findPath(root->left, value, path)) return true;
+        path.pop_back();
+        path.push_back('R');
+        if(findPath(root->right, value, path)) return true;
+        path.pop_back();
+        return false;
+    }
+
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string startPath, destPath;
+        findPath(root, startValue, startPath);
+        findPath(root, destValue, destPath);
+        int i = 0;
+        while(i < startPath.size() && i < destPath.size() && startPath[i] == destPath[i]) {
+            i++;
+        }
+        string ans;
+        for(int j = i; j < startPath.size(); j++) {
+            ans += 'U';
+        }
+        for(int j = i; j < destPath.size(); j++) {
+            ans += destPath[j];
+        }
+        return ans;
+    }
+};
