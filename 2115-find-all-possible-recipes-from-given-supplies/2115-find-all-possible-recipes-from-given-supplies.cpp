27aa5eac7d264b15427a744ef6883ada3e937d2a
Kavya Agarwal <kavyaag1617@gmail.com>
2026-08-31 06:56:10 +0530

    Time: 83 ms (75.78%), Space: 111.7 MB (65.74%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_set<string> avail;
-        for(string s : supplies) {
-            avail.insert(s);
+        int n = recipes.size();
+        unordered_map<string, int> id;
+        for(int i = 0; i < n; i++) {
+            id[recipes[i]] = i;
+        }
+        unordered_map<string, vector<int>> graph;
+        vector<int> need(n);
+        for(int i = 0; i < n; i++) {
+            need[i] = ingredients[i].size();
+            for(string x : ingredients[i]) {
+                graph[x].push_back(i);
+            }
+        }
+        queue<string> q;
+        for(string x : supplies) {
+            q.push(x);
         }
         vector<string> ans;
-        bool flag = true;
-        while(flag) {
-            flag = false;
-            for(int i = 0; i < recipes.size(); i++) {
-                if(avail.count(recipes[i])) continue;
-                bool possible = true;
-                for(string x : ingredients[i]) {
-                    if(!avail.count(x)) {
-                        possible = false;
-                        break;
-                    }
-                }
-                if(possible) {
-                    avail.insert(recipes[i]);
-                    ans.push_back(recipes[i]);
-                    flag = true;
+        while(!q.empty()) {
+            string x = q.front();
+            q.pop();
+            for(int recipe : graph[x]) {
+                need[recipe]--;
+                if(need[recipe] == 0) {
+                    ans.push_back(recipes[recipe]);
+                    q.push(recipes[recipe]);
                 }
             }
         }
