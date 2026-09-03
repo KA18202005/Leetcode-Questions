4e9e8b218016996dd54180e2e1e8514caae379c1
Kavya Agarwal <kavyaag1617@gmail.com>
2026-09-03 07:27:31 +0530

    Time: 75 ms (31.46%), Space: 153.9 MB (19.16%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -0,0 +1,29 @@
+class DetectSquares {
+    int cnt[1001][1001] = {};
+    vector<pair<int, int>> p;
+public:
+    DetectSquares() {
+        
+    }
+    
+    void add(vector<int> point) {
+        cnt[point[0]][point[1]]++;
+        p.emplace_back(point[0], point[1]);
+    }
+    
+    int count(vector<int> point) {
+        int x1 = point[0], y1 = point[1], ans = 0;
+        for (auto& [x3, y3] : p) {
+            if (abs(x1 - x3) == 0 || abs(x1 - x3) != abs(y1 - y3)) continue;
+            ans += cnt[x1][y3] * cnt[x3][y1];
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
