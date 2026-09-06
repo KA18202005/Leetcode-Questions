0ad038d755fea70f79ede6e11aea08a49487c8b8
Kavya Agarwal <kavyaag1617@gmail.com>
2026-09-06 07:20:19 +0530

    Time: 11 ms (87.61%), Space: 8.7 MB (99.22%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    const int prime = 1e9 + 7;
+public:
+    int numDistinct(string s, string t) {
+        int n = s.length(), m = t.length();
+        vector<int> prev(m + 1, 0);
+        prev[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s[i - 1] == t[j - 1]) {
+                    prev[j] = (prev[j - 1] + prev[j]) % prime;
+                }
+            }
+        }
+        return prev[m];
+    }
+};
