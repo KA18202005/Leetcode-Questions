6244ea654abde29ddfa9aa0d377546bf4afad091
Kavya Agarwal <kavyaag1617@gmail.com>
2026-09-10 07:20:57 +0530

    Time: 0 ms (100%), Space: 10.8 MB (87.85%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        set<int> st;
-        for(int i = 0; i < nums.size(); i++) 
-            if(nums[i] != 0) st.insert(nums[i]);
-        return st.size();
+        vector<int> freq(101, -1);
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); i++) freq[nums[i]]++;
+        for(int i = 1; i <= 100; i++) {
+            if(freq[i] != -1) cnt++;
+        }
+        return cnt;
     }
 };
