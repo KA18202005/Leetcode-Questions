abcb3b4594f6bbfc582493c2172aecbbc60d53e3
Kavya Agarwal <kavyaag1617@gmail.com>
2026-09-16 07:23:08 +0530

    Time: 0 ms (100%), Space: 9.9 MB (75.7%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,18 +1,29 @@
 class Solution {
     using ll = long long;
     const ll mod = 1e9 + 7;
-    vector<vector<ll>> dp;
 public:
-    ll solve(int n, int k){
-        if(k == 0 || k == n) return 1;
-        if(dp[n][k] != -1) return dp[n][k];
-        return dp[n][k] = (solve(n - 1, k - 1) + solve(n - 1, k)) % mod;
+    ll power(ll a, ll b) {
+        ll res = 1;
+        while (b) {
+            if (b & 1) res = res * a % mod;
+            a = a * a % mod;
+            b >>= 1;
+        }
+        return res;
     }
 
     int numberOfSets(int n, int k) {
         int N = n + k - 1;
         int R = 2 * k;
-        dp.assign(N + 1, vector<ll>(R + 1, -1));
-        return solve(N, R);
+        vector<ll> fact(N + 1), invFact(N + 1);
+        fact[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            fact[i] = fact[i - 1] * i % mod;
+        }
+        invFact[N] = power(fact[N], mod - 2);
+        for (int i = N; i >= 1; i--) {
+            invFact[i - 1] = invFact[i] * i % mod;
+        }
+        return fact[N] * invFact[R] % mod * invFact[N - R] % mod;
     }
 };
